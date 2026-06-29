0cf8a3ef452551463e8be3a57f9487c215d62350
cesarjlfit-create <cesar.jlfit@gmail.com>
2026-06-29 17:01:14 -0300

    Add files via upload

---
+++ api.ts
@@ -0,0 +1,26 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
+
+const API_BASE = '/api';
+
+export async function fetchApi(endpoint: string, options: RequestInit = {}) {
+  const res = await fetch(`${API_BASE}${endpoint}`, {
+    ...options,
+    headers: {
+      ...(options.body instanceof FormData ? {} : { 'Content-Type': 'application/json' }),
+      ...options.headers,
+    },
+  });
+  
+  if (!res.ok) {
+    let message = 'Erro na requisição';
+    try {
+      const data = await res.json();
+      message = data.mensagem || message;
+    } catch (e) {}
+    throw new Error(message);
+  }
+  
+  return res.json();
+}
