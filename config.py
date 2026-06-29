b36cbc5eac036cee9b95866ac0a3fbbe1bed5fb5
cesarjlfit-create <cesar.jlfit@gmail.com>
2026-06-29 12:00:49 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+# Ficheiro config.py
+SUPABASE_URL = "https://ntzcnxylfxpohvijbfhh.supabase.co"
+SUPABASE_KEY = "sb_publishable_fhe8MT15qIcIVIIbvdsrCw_pYCMjCDz"
+GEMINI_API_KEY = "AQ.Ab8RN6Iu4N6ugfvwA1l6HaNLS3FnE4ib9w-MobNKGLU55aSK5w"
